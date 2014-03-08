2e59676d4903f7c302f8da10a4d3f96308dd1970
Adrien Cahen <onlineallthetime@gmail.com>
2014-03-07 17:24:54 -0800

    inception test

--- test/qunit/browserify.qunit.js
+++ test/qunit/browserify.qunit.js
@@ -1,33 +1,29 @@
 module('Browserify');
 
 test('CommonJS', function() { 
-    ok(typeof window.require === "function", 'require is a function');
+    ok(typeof window.require === "function", 'require is a global function');
+
+    propEqual(exports, {}, 'not exporting anything');
+
+    var qunit = require('qunitjs');
+    equal(module, qunit.module, 'in this context, module is from qunit');
+    equal(equal, qunit.equal, 'inception');
 });
 
 
-test('jQuery', function() { 
+test('Libraries', function() { 
     var jQuery = require('jquery');
     equal(jQuery, window.$, 'same as window.$');
 
     var v = jQuery().jquery;
     ok(v, 'v'+v);
-});
 
 
-test('Underscore', function() {
-    var v = require('underscore').VERSION;
-    ok(v, 'v'+v);
-});
+    var libs = ['underscore','backbone','handlebars'];
 
+    for (var i = 0; i < libs.length; i++) {
+        v = require(libs[i]).VERSION;
+        ok(v, libs[i]+' v'+v);
+    }
 
-test('Backbone', function() {
-    var v = require('backbone').VERSION;
-    ok(v, 'v'+v);
-});
-
-
-
-test('Handlebars', function() {
-    var v = require('handlebars').VERSION;
-    ok(v, 'v'+v);
 });
